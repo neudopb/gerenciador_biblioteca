2e6ffbf7d2c977a6cb44fc0938bfa6c4b4bd76db
neudopb <neudinho66@gmail.com>
2021-12-24 16:35:48 -0300

    models accounts

--- accounts/models.py
+++ accounts/models.py
@@ -32,7 +32,7 @@ class Usuario(AbstractUser):
     is_staff =models.BooleanField('Membro da equipe', default=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', telefone]
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
