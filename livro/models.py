60570f592f9ceb5f8feeedccd9afa6fe152e6ef8
neudopb <neudinho66@gmail.com>
2021-12-24 08:01:28 -0300

    Models de usuario e Livro

---
+++ livro/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Livros(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    coautor = models.CharField(max_length=50, blank=True)
+    data_cadastro = models.DateField(default = timezone.now)
+    emprestado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Livro"
